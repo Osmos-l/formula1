1fb8da034ba83221c31c47b8146a8b1d8634c25f
Jordan LECLERCQ <emailjeux81@gmail.com>
2024-09-28 15:47:49 +0200

    feat: MeetingPreview minors

--- src/components/MeetingPreview/index.tsx
+++ src/components/MeetingPreview/index.tsx
@@ -9,18 +9,15 @@ interface MeetingPreviewProps {
 export default function MeetingPreview({ meeting, session }: MeetingPreviewProps) {
     return (
         <div className='p-2'>
-            <div>
-                <h1>Meeting: </h1>
-                <p className='px-5'>
-                    {meeting?.location} - {meeting?.meeting_name} ({meeting?.meeting_code} {meeting?.year})<br />
-                    { session && (
-                        <>
-                        {session.session_name} session:  ({convertToHumanReadableDate(session.date_start)})<br />
-                        At {convertToHumanReadableTime(session.date_start)} to {convertToHumanReadableTime(session.date_end)}
-                        </>
-                    ) }
-                </p>
-            </div>
+            <h1>{meeting?.location} - {meeting?.meeting_name} ({meeting?.meeting_code} {meeting?.year})</h1>
+            <p className='px-5'>
+                { session && (
+                    <>
+                    {session.session_name} session<br />
+                    The {convertToHumanReadableDate(session.date_start)} starting at {convertToHumanReadableTime(session.date_start)}
+                    </>
+                ) }
+            </p>
         </div>
     );
 }
